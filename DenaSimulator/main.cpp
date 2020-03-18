edd618d6f3b347a8843d23749b21634a8f59808e
Biao Chen <biaochen@cmail.carleton.ca>
2020-03-17 23:58:17 -0400

    added main page menu for dena simulator

---
+++ DenaSimulator/main.cpp
@@ -0,0 +1,11 @@
+#include "denasimulator.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DenaSimulator w;
+    w.show();
+    return a.exec();
+}
